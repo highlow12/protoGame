28acdf94803e226f730b5eb7c38c37fffbddf80b
highlow12 <highlow12@naver.com>
2021-01-20 17:44:41 +0900

    2020/01/20

---
+++ New Unity Project/Assets/move.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] objects = new GameObject[5];
+    public int position = 0;
+
+    int a;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(objects.Length);
+        a = objects.Length;
+        position--;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, objects[position].transform.position, speed * Time.deltaTime);
+        if (Input.anyKeyDown)
+        {
+            
+            
+            if (position <=0)
+            {
+                position = a-1;
+            }
+            else
+            {
+                position--;
+            }
+
+        }
+    }
+}
